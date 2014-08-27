969f8a2b8a724196063585f4845772cf12c2cbae
Riccardo Bruno <riccardo.bruno@gmail.com>
2014-08-27 15:51:48 +0200

    Updating to v1.1

--- showqr.py
+++ showqr.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python2.7
-import tempfile
-from os  import remove 
 from sys import stdin
-from subprocess import Popen, PIPE
-from qrcode import *
+from qrcode.main import QRCode, make
+from qrcode.constants import *
+from qrcode import image
+import ImageFont
+import ImageDraw
+
+VER="1.1"
+SHOWTXT=True
+FONT='/Library/Fonts/Courier New.ttf'
+FONTSZ=12
+BANNER='http://ricsxn.duckdns.org/donate.html'
+HEADER='ShowQR v%s' % VER
 
 qr = QRCode()
 for line in stdin:
     qr.add_data(line)
 qr.make()
 im = qr.make_image()
-temp = tempfile.NamedTemporaryFile(dir='/tmp')
-im.save(temp.name)
-pr=Popen("{}; {}".format('open %s' % temp.name,'sleep 5'),shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
-stdout,stderr = pr.communicate()
+if SHOWTXT is True:
+	font = ImageFont.truetype(FONT, FONTSZ, encoding='unic')
+	text = BANNER.decode('utf-8')
+	head = HEADER.decode('utf-8')
+	(txt_width, txt_height) = font.getsize(text)
+	(hdr_width, hdr_height) = font.getsize(head)
+	(qr_width,qr_height) = im.size
+	draw = ImageDraw.Draw(im)
+	draw.text(((qr_width-hdr_width)/2, 1),head,font=font)
+	draw.text(((qr_width-txt_width)/2, qr_height-txt_height-1),text,font=font) 
+im.show()
